3ecbd0320c423bafe81c0288f97c50fd325b3573
Sergei <voloboeff2010@yandex.ru>
2021-08-03 15:10:57 +0500

    fix of trainer of irregular verbs

--- lingoni GERMAN/irregular_verbs_trainer.py
+++ lingoni GERMAN/irregular_verbs_trainer.py
@@ -1,6 +1,6 @@
 from os.path import exists, join, splitext
 from os import listdir
-from random import sample, randrange
+from random import sample, randrange, shuffle
 verb_folder = 'irregular verbs'
 
 groups_dict = dict()
@@ -31,17 +31,22 @@ difficulty = int(input("Выбери сложность от 1 до 3:"))
 
 
 all_verbs = [x for y in [groups_dict[k] for k in selected_groups] for x in y]
+shuffle(all_verbs)
+
 ans = ''
 correct_count = 0
 all_count = 0
+chosen_verb = 0
 while ans != 'stop':
     if difficulty != 3:
         exclude_forms = sample(list(range(4)), difficulty)
     else:
         exclude_forms = [1,2,3]
-    chosen_verb = randrange(len(all_verbs))
+   # chosen_verb = randrange(len(all_verbs))
+
     forms = all_verbs[chosen_verb][:3]
     translation = all_verbs[chosen_verb][3]
+    chosen_verb = (chosen_verb + 1) % len(all_verbs)
     forms_to_print = [('Translation: ', translation),
         ('Infinitiv: ', forms[0]), ('Präteritum: ', forms[1]), ('Partizip 2: ', forms[2])]
     for i in range(4):
