254220e34f0e21c35ebf863c5b5683f595b454c7
Sergei <voloboeff2010@yandex.ru>
2021-08-05 17:37:38 +0300

    rounds added, but some bugs remains

--- lingoni GERMAN/irregular_verbs_trainer.py
+++ lingoni GERMAN/irregular_verbs_trainer.py
@@ -1,16 +1,19 @@
 from os.path import exists, join, splitext
 from os import listdir
 from random import sample, randrange, shuffle
+
 verb_folder = 'irregular verbs'
 
 groups_dict = dict()
 
+
 def parse_str(s: str):
     return [x.replace('"', '').lower() for x in list(filter(lambda x: len(x) > 0, s[:-1].split(',')))]
 
+
 for p in listdir(verb_folder):
     groups_dict[splitext(p)[0]] = list()
-    with open(join(verb_folder, p), 'r') as f:
+    with open(join(verb_folder, p), 'r', encoding='utf-8') as f:
         f.readline()
         for l in f.readlines():
             tmp = parse_str(l)
@@ -29,26 +32,44 @@ print('Выбранны группы', *selected_groups, sep='\n')
 print('-------------------------------------------')
 difficulty = int(input("Выбери сложность от 1 до 3:"))
 
-
 all_verbs = [x for y in [groups_dict[k] for k in selected_groups] for x in y]
+success_map = dict(zip([x[3] for x in all_verbs], [[] for i in range(len(all_verbs))]))
+
 shuffle(all_verbs)
 
 ans = ''
 correct_count = 0
 all_count = 0
 chosen_verb = 0
+errors_round = False
 while ans != 'stop!':
     if difficulty != 3:
         exclude_forms = sample(list(range(4)), difficulty)
     else:
-        exclude_forms = [1,2,3]
-   # chosen_verb = randrange(len(all_verbs))
+        exclude_forms = [1, 2, 3]
+    # chosen_verb = randrange(len(all_verbs))
 
     forms = all_verbs[chosen_verb][:3]
     translation = all_verbs[chosen_verb][3]
-    chosen_verb = (chosen_verb + 1) % len(all_verbs)
+    chosen_verb = chosen_verb + 1
+    if chosen_verb == len(all_verbs):
+        failed_verbs = set([k for k, v in success_map.items() if sum(v) > 0])
+        if not errors_round and len(failed_verbs) > 0:
+            print('errors round')
+            all_verbs = [x for y in [groups_dict[k] for k in selected_groups] for x in y]
+            if len(failed_verbs) > 0:
+                all_verbs = [x for x in all_verbs if x[3] in failed_verbs]
+        else:
+            print('all verbs round')
+            all_verbs = [x for y in [groups_dict[k] for k in selected_groups] for x in y]
+            success_map = dict(zip([x[3] for x in all_verbs], [[] for i in range(len(all_verbs))]))
+            shuffle(all_verbs)
+
+        errors_round = not errors_round
+        chosen_verb = 0
+    chosen_verb = chosen_verb % len(all_verbs)
     forms_to_print = [('Translation: ', translation),
-        ('Infinitiv: ', forms[0]), ('Präteritum: ', forms[1]), ('Partizip 2: ', forms[2])]
+                      ('Infinitiv: ', forms[0]), ('Präteritum: ', forms[1]), ('Partizip 2: ', forms[2])]
     for i in range(4):
         if i not in exclude_forms:
             print(f'{forms_to_print[i][0]:15s}', forms_to_print[i][1])
@@ -60,9 +81,11 @@ while ans != 'stop!':
         all_count += 1
         ans = ans.strip().lower()
         if ans != forms_to_print[form][1]:
+            success_map[translation].append(1)
             print('WRONG!!!')
             print('correct was: ', forms_to_print[form][1])
         else:
+            success_map[translation].append(0)
             correct_count += 1
             print('correct, bro!')
     if ans == 'stop!':
